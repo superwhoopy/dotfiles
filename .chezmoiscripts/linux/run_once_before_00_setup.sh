b2b81586d8c640c9de8c9b43dd3808fd3787db7b
Emmanuel Ohayon <eohayon@mail.ks.int>
2024-10-06 17:46:15 +0200

    WIP

--- .chezmoiscripts/linux/run_once_before_00_setup.sh
+++ .chezmoiscripts/linux/run_once_before_00_setup.sh
@@ -31,23 +31,23 @@ if ! $(/bin/grep --silent mattermost.ks.int /etc/hosts);
 then
   _blue "Editing /etc/hosts file"
   sudo -- sh << EOF
-  (
-    echo "# Internal servers ###################"
-    echo "10.191.2.10  intra.ks.int"
-    echo "10.191.2.63  mattermost.ks.int"
-    echo "10.191.2.24  front.jenkins.ks.int"
-    echo "10.191.2.26  license.ks.int"
-    echo "10.191.2.28  core.jenkins.ks.int"
-    echo "10.191.2.35  dev.ks.int"
-    echo "10.191.2.56  polarion.ks.int"
-    echo "10.191.2.37  mail.ks.int"
-    echo "10.191.2.38  checker.jenkins.ks.int"
-    echo "10.191.2.40  designer.jenkins.ks.int"
-    echo "10.191.2.22  dal.jenkins.ks.int"
-    echo "10.191.2.42  store.ks.int"
-    echo "10.191.2.43  doc.ks.int"
-    echo "10.191.2.64  haas.ks.int"
-    echo "######################################"
-  ) >> /etc/hosts
-  EOF
+(
+  echo "# Internal servers ###################"
+  echo "10.191.2.10  intra.ks.int"
+  echo "10.191.2.63  mattermost.ks.int"
+  echo "10.191.2.24  front.jenkins.ks.int"
+  echo "10.191.2.26  license.ks.int"
+  echo "10.191.2.28  core.jenkins.ks.int"
+  echo "10.191.2.35  dev.ks.int"
+  echo "10.191.2.56  polarion.ks.int"
+  echo "10.191.2.37  mail.ks.int"
+  echo "10.191.2.38  checker.jenkins.ks.int"
+  echo "10.191.2.40  designer.jenkins.ks.int"
+  echo "10.191.2.22  dal.jenkins.ks.int"
+  echo "10.191.2.42  store.ks.int"
+  echo "10.191.2.43  doc.ks.int"
+  echo "10.191.2.64  haas.ks.int"
+  echo "######################################"
+) >> /etc/hosts
+EOF
 fi
