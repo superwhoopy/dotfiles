4326f5f5cc48c1a6b7ff08aa83eab95529cb7c82
Emmanuel Ohayon <eohayon@mail.ks.int>
2024-10-07 15:00:23 +0200

    WIP

--- dot_config/test/runtest.ps1
+++ dot_config/test/runtest.ps1
@@ -3,5 +3,4 @@ $ErrorActionPreference = "Stop"
 Set-StrictMode -Version Latest
 
 # download chezmoi and apply, see how it goes
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 iex "&{$(irm 'https://get.chezmoi.io/ps1')} -- init --apply superwhoopy"
