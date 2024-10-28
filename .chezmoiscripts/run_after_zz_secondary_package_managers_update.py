a7428b2d61f606b6147be1adb0370cfd75e67818
Emmanuel Ohayon <eohayon@mail.ks.int>
2024-10-28 11:42:55 +0100

    async/await implem of Python script + add rustup

--- .chezmoiscripts/run_after_zz_secondary_package_managers_update.py
+++ .chezmoiscripts/run_after_zz_secondary_package_managers_update.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """TODO"""
 
-import subprocess
-import threading
+import asyncio
 import shutil
 import typing
 
@@ -23,6 +22,8 @@ UPDATE_COMMANDS = {
 
     'pipx': ('upgrade-all',),
 
+    'rustup': ('update',),
+
     # 'gem': ('update',),
 }
 
@@ -40,66 +41,57 @@ class ProgressContext(typing.NamedTuple):
     task_id: int
 
 
-def _run_command(lock: threading.Lock, progress: ProgressContext, cmd, args,
-                 output: list):
-    with lock:
-        progress.ctx.start_task(progress.task_id)
-    if (fullcmd := shutil.which(cmd)) is None:
-        with lock:
-            progress.ctx.remove_task(progress.task_id)
+async def _run_command(progress: ProgressContext, cmd, args):
+    progress.ctx.start_task(progress.task_id)
+
+    fullcmd = await asyncio.to_thread(shutil.which, cmd)
+    if fullcmd is None:
+        progress.ctx.remove_task(progress.task_id)
         return
 
     if isinstance(args[0], str):
         args = (args,)
 
+    output = []
     for a in args:
-        command = [fullcmd, *a]
-        out = subprocess.run(command, check=False, stdout=subprocess.PIPE,
-                             stderr=subprocess.STDOUT)
+        proc = await asyncio.create_subprocess_exec(
+            fullcmd, *a,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.STDOUT)
+        stdout, _ = await proc.communicate()
+        output.append(stdout.decode())
 
-    with lock:
-        output.append(out.stdout.decode())
-        # TODO: update progress bar with a notification when the command failed
-        progress.ctx.advance(progress.task_id, 100)
+    # TODO: update progress bar with a notification when the command failed
+    progress.ctx.advance(progress.task_id, 100)
+    return output
 
 
-def main():
+async def main():
     _chezmoiprint('Secondary package managers update')
-    global_lock = threading.Lock()
 
     with Progress(
         TextColumn("{task.description}", table_column=Column(ratio=1)),
         SpinnerColumn(spinner_name='bouncingBall', finished_text='✅'),
         TimeElapsedColumn(),
-        transient=True
+        transient=False
     ) as progress:
 
-        threads_output = []
-
-        threads = tuple(
-            threading.Thread(
-              target=_run_command,
-              args=(global_lock,
+        async with asyncio.TaskGroup() as tg:
+            _ = tuple(
+                tg.create_task(_run_command(
                     ProgressContext(progress,
                                     progress.add_task(f'{cmd}...',
                                                       total=100,
                                                       start=False)),
-                    cmd, args, threads_output)
+                    cmd, args))
+                for cmd, args in UPDATE_COMMANDS.items()
             )
-            for cmd, args in UPDATE_COMMANDS.items()
-        )
 
-        for t in threads:
-            t.start()
-
-        for t in threads:
-            t.join()
-
-        # for o in threads_output:
-        #     print(o)
+        # for t in tasks:
+        #     print(t.result())
 
         print('...completed')
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
