dc30328c64db8c66ab5f7b61b06fdafcc7e5c4a5
Emmanuel Ohayon <eohayon@mail.ks.int>
2024-11-22 15:26:14 +0100

    proj: rtk-p1011 gen-compile-commands

---
+++ projects/rtk-p1011/gen-compile-commands.py
@@ -0,0 +1,54 @@
+"""Generate a compile_commands.json (Compilation Database file) from the
+"Compilation Summary" JSON files in the repository.
+
+Usage: python gen-compile-commands.py > compile_commands.json
+"""
+
+import json
+
+from pathlib import Path
+
+CFLAGS = ["--std=c89", "-Wall", "-Wextra", "-D_DIAB_TOOL", "-DAST_ARCH_E500"]
+
+COMP_SUMMARY_JSON = Path(
+        'bundle/compilation_summary_power-qoriq-p1011-ganav800-p.json')
+
+SOURCE_COMP_SUMMARY_JSON = Path(
+        'source_bundle/source_compilation_summary.json')
+
+HERE = Path(__file__).parent
+
+
+def main():
+    with COMP_SUMMARY_JSON.open('r') as csf:
+        compilation_summary = json.loads(csf.read())
+    iflags = [f"-Ibundle/{incdir}"
+              for incdir in compilation_summary['include_dirs']]
+    db = [
+        {
+            "directory": str(HERE),
+            "arguments": ['clang'] + CFLAGS + iflags + [f"bundle/{srcfile}"],
+            "file": f"bundle/{srcfile}",
+        }
+        for srcfile in compilation_summary['c_sources']
+    ]
+
+    with SOURCE_COMP_SUMMARY_JSON.open('r') as scsf:
+        compilation_summary = json.loads(scsf.read())
+    iflags = [f"-Isource_bundle/{incdir}"
+              for incdir in compilation_summary['sources'][0]['include_dirs']]
+    db.extend([
+        {
+            "directory": str(HERE),
+            "arguments": (['clang'] + CFLAGS + iflags
+                          + [f"source_bundle/{srcfile}"]),
+            "file": f"source_bundle/{srcfile}",
+        }
+        for srcfile in compilation_summary['sources'][0]['c_sources']
+    ])
+
+    print(json.dumps(db, indent=2))
+
+
+if __name__ == "__main__":
+    main()
