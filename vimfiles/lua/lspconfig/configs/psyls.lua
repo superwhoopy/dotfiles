9b30cac2a4c25916e79dffd44353156427b00ce8
Emmanuel Ohayon <eohayon@mail.ks.int>
2025-01-29 23:53:04 +0100

    vim: psyls config

--- vimfiles/lua/lspconfig/configs/psyls.lua
+++ vimfiles/lua/lspconfig/configs/psyls.lua
@@ -1,9 +1,10 @@
 local util = require 'lspconfig.util'
 
+local psyls_bin = (vim.fn.has('win32') == 1) and 'psy-ls.exe' or 'psy-ls'
 return {
   default_config = {
     name = 'psy-ls',
-    cmd = {vim.fn.expand('$HOME/workspace/psyls/target/debug/psy-ls.exe')},
+    cmd = {vim.fn.expand('$HOME/workspace/psyls/target/debug/' .. psyls_bin)},
     filetypes = {'psy'},
     root_dir = function(_)
       return vim.fs.root(0, {'workspace.toml'})
