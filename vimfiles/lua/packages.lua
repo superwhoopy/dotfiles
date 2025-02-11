f72c8bed904f8e21539bfd8478febb93bdee6e1a
Emmanuel Ohayon <eohayon@mail.ks.int>
2025-02-11 09:20:18 +0100

    vim: remove tsplayground (deprecated)

--- vimfiles/lua/packages.lua
+++ vimfiles/lua/packages.lua
@@ -153,29 +153,6 @@ local function nvim_treesitter_fn()
   }
 end
 
-local function nvim_treesitter_playgroung_fn()
-  require'nvim-treesitter.configs'.setup {
-    playground = {
-      enable = true,
-      disable = {},
-      updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
-      persist_queries = false, -- Whether the query persists across vim sessions
-      keybindings = {
-        toggle_query_editor = 'o',
-        toggle_hl_groups = 'i',
-        toggle_injected_languages = 't',
-        toggle_anonymous_nodes = 'a',
-        toggle_language_display = 'I',
-        focus_language = 'f',
-        unfocus_language = 'F',
-        update = 'R',
-        goto_node = '<cr>',
-        show_help = '?',
-      },
-    }
-  }
-end
-
 -- NO NECK PAIN ----------------------------------------------------------------
 local noneckpain_opts = {
   autocmds = {
@@ -661,7 +638,6 @@ P.plugins = {
     build=':TSUpdate',
     config=nvim_treesitter_fn
   },
-  {'nvim-treesitter/playground', config=nvim_treesitter_playgroung_fn},
 
   'psliwka/vim-smoothie', -- smooth scrolling
   "rafamadriz/friendly-snippets", -- snippets
