843faf1d18a7b87c6d6729a0ba1d6f6c258e3039
Emmanuel Ohayon <eohayon@mail.ks.int>
2026-05-18 09:17:28 +0200

    vim: default conf for vim-ls

--- vimfiles/lua/packages.lua
+++ vimfiles/lua/packages.lua
@@ -464,7 +464,7 @@ local function lspconfig_fn()
         runtime = {
           -- Tell the language server which version of Lua you're using (most
           -- likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
+          -- version = 'LuaJIT',
         },
         diagnostics = {
           -- Get the language server to recognize the `vim` global
